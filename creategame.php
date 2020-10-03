b23a71e07d55bc6136a9f23ac70e2ecbca1e76ea
hnuttapon <42200290+hnuttapon@users.noreply.github.com>
2020-10-03 17:15:42 +0700

    fix create game5

--- creategame.php
+++ creategame.php
@@ -1,7 +1,7 @@
 <?php
- //  $config = include('config.php');
- //  $con = mysqli_connect($config['host'], $config['username'], $config['password'], $config['databasename']);
- $con = mysqli_connect('eu-cdbr-west-03.cleardb.net', 'bf00e4ec332952', '96cb1300', 'heroku_e2c4947f07c47f2');
+  $config = include('config.php');
+  $con = mysqli_connect($config['host'], $config['username'], $config['password'], $config['databasename']);
+  $con = mysqli_connect('eu-cdbr-west-03.cleardb.net', 'bf00e4ec332952', '96cb1300', 'heroku_e2c4947f07c47f2');
  if(mysqli_connect_errno()){
      echo "1: Connection failed"; 
      exit();

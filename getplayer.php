087d6dca55cac94e90a62db883833101634e14fb
hnuttapon <42200290+hnuttapon@users.noreply.github.com>
2020-10-04 17:09:50 +0700

    get game

--- getplayer.php
+++ getplayer.php
@@ -4,8 +4,8 @@
         echo "1: Connection failed"; //error code #1 = connection failed
         exit();
     }
-
-    $sql = "SELECT player FROM game";
+    $gamename = $_POST["gamename"];
+    $sql = "SELECT player FROM game WHERE GameName = '" . $gamename . "';";
     $result = $con->query($sql) or die("query failed");
     echo "player:";
     echo $result->num_rows . ":";

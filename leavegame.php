b071aab78675d6945f8c98a5fb56d5b171bda010
hnuttapon <42200290+hnuttapon@users.noreply.github.com>
2020-10-04 16:34:39 +0700

    leavegame

--- leavegame.php
+++ leavegame.php
@@ -11,24 +11,24 @@
     $gamename = $_POST["gamename"];
 
     if($username == $gamename){
-        $deletegamequery = "DELETE FROM currentgame WHERE GameName = $gamename;";
-        mysqli_query($con, $deletegamequery) or die("1: delete game query failed " . mysqli_error($con)); 
+        $deletegamequery = "DELETE FROM currentgame WHERE GameName = '" . $gamename . "';";
+        mysqli_query($con, $deletegamequery) or die("1: " . mysqli_error($con)); 
     }
     else if($username != $gamename){
 
         $gamecheckquery = "SELECT * FROM currentgame WHERE GameName = '" . $gamename . "';";
-        $gamecheck = mysqli_query($con, $gamecheckquery) or die("2 :select totalplayer query failed" . mysqli_error($con));
+        $gamecheck = mysqli_query($con, $gamecheckquery) or die("2: " . mysqli_error($con));
         
         if ($gamecheck->num_rows > 0) {
             while($row = $gamecheck->fetch_assoc()) {
               $totalplayer = $row["TotalPlayer"];
             }
             if($totalplayer == 1){
-                $deletegamequery = "DELETE FROM currentgame WHERE GameName = $gamename;";
+                $deletegamequery = "DELETE FROM currentgame WHERE GameName = '" . $gamename . "';";
                 mysqli_query($con, $deletegamequery) or die("3: delete game query failed " . mysqli_error($con)); 
             }
             else{
-                $deletequery = "DELETE FROM game WHERE player = $username;";
+                $deletequery = "DELETE FROM game WHERE player = '" . $username . "';";
                 mysqli_query($con, $deletequery) or die("4: delete query failed " . mysqli_error($con)); 
 
                 $totalplayer = $totalplayer - 1;

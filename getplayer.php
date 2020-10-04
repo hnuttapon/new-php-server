cee8acd1cb5f170a229750dc21a6f47d7bb43e55
hnuttapon <42200290+hnuttapon@users.noreply.github.com>
2020-10-04 17:22:27 +0700

    get game

--- getplayer.php
+++ getplayer.php
@@ -5,7 +5,7 @@
         exit();
     }
     $gamename = $_POST["gamename"];
-    $sql = "SELECT player FROM game WHERE GameName = '" . $gamename . "';";
+    $sql = "SELECT player FROM game WHERE GameName = '" . $gamename . "'";
     $result = $con->query($sql) or die("query failed");
     echo "player:";
     echo $result->num_rows . ":";

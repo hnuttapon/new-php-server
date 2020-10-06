0fa6b155b9f2a70ba5990ec82fae5f3ab781ed67
hnuttapon <42200290+hnuttapon@users.noreply.github.com>
2020-10-06 16:37:38 +0700

    update database

--- register.php
+++ register.php
@@ -1,6 +1,4 @@
 <?php
- //  $config = include('config.php');
- //  $con = mysqli_connect($config['host'], $config['username'], $config['password'], $config['databasename']);
  $con = mysqli_connect('mysql-13571-0.cloudclusters.net', 'pon', '1234', 'mysqlcluster');
  if(mysqli_connect_errno()){
      echo "1: Connection failed" . mysqli($con); //error code #1 = connection failed

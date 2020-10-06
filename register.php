b4aeee61b65ddd0381a68d61ec3bc5d4a4272d7b
hnuttapon <42200290+hnuttapon@users.noreply.github.com>
2020-10-06 21:19:45 +0700

    update database

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
-   $con = mysqli_connect('eu-cdbr-west-03.cleardb.net', 'bf00e4ec332952', '96cb1300', 'heroku_e2c4947f07c47f2');
+   $con = mysqli_connect('mysql-13571-0.cloudclusters.net:13572', 'pon', '1234', 'mysqlcluster');
    if(mysqli_connect_errno()){
        echo "1: Connection failed"; //error code #1 = connection failed
        exit();
